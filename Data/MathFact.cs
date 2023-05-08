dbc22a51564972fcfae4aacbb73b60a080cfc288
Ryan Palumbo <rynpal@gmail.com>
2023-05-08 08:30:17 -0400

    first commit

---
+++ Data/MathFact.cs
@@ -0,0 +1,17 @@
+using System.Text;
+
+namespace MathFacts.Data;
+
+public class MathFact
+{
+    public int FirstNum { get; set; }
+    public int SecondNum { get; set; }
+    public string? Operator { get; set; }
+
+    public MathFact()
+    {
+        this.FirstNum = int.MinValue;
+        this.SecondNum = int.MinValue;
+        this.Operator = string.Empty;
+    }
+}
