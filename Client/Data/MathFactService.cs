e4e590246cb0cd6bff99603a46f06b93e689ede8
Ryan Palumbo <rynpal@gmail.com>
2023-05-08 08:48:15 -0400

    creating files in Client directory

---
+++ Client/Data/MathFactService.cs
@@ -0,0 +1,34 @@
+namespace MathFacts.Data;
+
+public class MathFactService
+{
+    public Task<MathFact[]> GetMultiplyFacts()
+    {
+        return Task.FromResult(Enumerable.Range(1, 96).Select(index => new MathFact
+        {
+            FirstNum = Random.Shared.Next(1, 12),
+            SecondNum = Random.Shared.Next(1, 12),
+            Operator = "x"
+        }).ToArray());
+    }
+
+    public Task<MathFact[]> GetAdditionFacts()
+    {
+        return Task.FromResult(Enumerable.Range(1, 96).Select(index => new MathFact
+        {
+            FirstNum = Random.Shared.Next(1, 20),
+            SecondNum = Random.Shared.Next(1, 20),
+            Operator = "+"
+        }).ToArray());
+    }
+
+    public Task<MathFact[]> GetSubtractionFacts()
+    {
+        return Task.FromResult(Enumerable.Range(1, 96).Select(index => new MathFact
+        {
+            FirstNum = Random.Shared.Next(1, 20),
+            SecondNum = Random.Shared.Next(1, 20),
+            Operator = "-"
+        }).ToArray());
+    }
+}
